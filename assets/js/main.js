c3eebfa8bf5f46ba238b0281548d28a431778798
T-sabljic <toni.majkic@gmail.com>
2023-03-05 19:35:08 +0100

    Structure and styles of Team,Blog and footer sections,+ scrollTop and JS ScrollTop

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,3 +13,43 @@ hamburger.addEventListener("click", openMobileMenu);
 function openMobileMenu(){
    list.classList.toggle("show");
 }
+
+//Projects sort
+
+let buttons =document.querySelectorAll(".project__btn");
+for( let button of buttons){
+    button.addEventListener("click", () =>{
+       let category = button.getAttribute("data-category");
+       let articles = document.querySelectorAll(".project__item")
+       
+       for(let article of articles){
+        article.style.display = "none";
+       }
+
+       for (let article of articles) {
+        if (article.getAttribute('data-category').includes(category)) {
+            article.style.display = 'block';
+        }
+    }
+
+    });
+}
+
+//Scroll To The Top
+
+let scroll = document.getElementById("scrollTop");
+scroll.addEventListener("click", myScroll);
+
+function myScroll(){
+    window({top:0, behavior:"smooth"});
+}
+
+window.addEventListener("scroll", showScroll);
+function showScroll(){
+    if (document.documentElement.scrollTop > 300){
+        scroll.classList.add('scrollShow');
+    }
+    else{
+        scroll.classList.remove('scrollShow');
+      }
+}
